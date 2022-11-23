63d57be7d8c890296ec21900680cd440fe2111c9
Михаил Кладько <116463567+Sadm1sh@users.noreply.github.com>
2022-11-23 17:33:49 +0300

    база данных музыка

--- Script.sql
+++ Script.sql
@@ -1,55 +1,64 @@
-CREATE TABLE IF NOT EXISTS Genres (
-    id SERIAL PRIMARY KEY
-    , Title VARCHAR(30) NOT NULL
+DROP TABLE genre_perfomer;
+DROP TABLE perfomer_album;
+DROP TABLE song_compilation;
+DROP TABLE genres;
+DROP TABLE performers;
+DROP TABLE songs;
+DROP TABLE albums;
+DROP TABLE compilations;
+CREATE TABLE IF NOT EXISTS genres (
+	id SERIAL PRIMARY KEY,
+	title VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS Performers (
-    id SERIAL PRIMARY KEY
-    , Name VARCHAR(30) NOT NULL
+CREATE TABLE IF NOT EXISTS performers (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS GenrePerfomer (
-    Genre_id INTEGER REFERENCES Genres(id)
-    , Perfomer_id INTEGER REFERENCES Performers(id)
-    , CONSTRAINT pk_GenrePerfomer PRIMARY KEY (
-        Genre_id
-        , Perfomer_id
-    )
+CREATE TABLE IF NOT EXISTS genre_perfomer (
+	genre_id INTEGER REFERENCES genres(id),
+	perfomer_id INTEGER REFERENCES performers(id),
+	CONSTRAINT pk_genre_perfomer PRIMARY KEY (
+		genre_id,
+		perfomer_id
+	)
 );
 
-CREATE TABLE IF NOT EXISTS Albums (
-    id SERIAL PRIMARY KEY
-    , Title VARCHAR(30) NOT NULL
-    , Year_of_issue INTEGER
+CREATE TABLE IF NOT EXISTS albums (
+	id SERIAL PRIMARY KEY,
+	title VARCHAR(30) NOT NULL,
+	year_of_issue INTEGER
 );
 
-CREATE TABLE IF NOT EXISTS PerfomerAlbum (
-    Perfomer_id INTEGER REFERENCES Performers(id)
-    , Album_id INTEGER REFERENCES Albums(id)
-    , CONSTRAINT pk_PerfomerAlbum PRIMARY KEY (
-        Perfomer_id
-        , Album_id
-    )
+CREATE TABLE IF NOT EXISTS perfomer_album (
+	perfomer_id INTEGER REFERENCES performers(id),
+	album_id INTEGER REFERENCES albums(id),
+	CONSTRAINT pk_perfomer_album PRIMARY KEY (
+		perfomer_id,
+		album_id
+	)
 );
 
-CREATE TABLE IF NOT EXISTS Songs (
-    id SERIAL PRIMARY KEY
-    , Title VARCHAR(30) NOT NULL
-    , Duration INTEGER
-    , Album_id INTEGER REFERENCES Albums(id)
+CREATE TABLE IF NOT EXISTS songs (
+	id SERIAL PRIMARY KEY,
+	title VARCHAR(30) NOT NULL,
+	duration TIME,
+	album_id INTEGER REFERENCES albums(id)
 );
 
-CREATE TABLE IF NOT EXISTS Compilations (
-    id SERIAL PRIMARY KEY
-    , Title VARCHAR(30) NOT NULL
-    , Year_of_issue INTEGER
+CREATE TABLE IF NOT EXISTS compilations (
+	id SERIAL PRIMARY KEY,
+	title VARCHAR(30) NOT NULL,
+	year_of_issue INTEGER
 );
 
-CREATE TABLE IF NOT EXISTS SongCompilation (
-    Song_id INTEGER REFERENCES Songs(id)
-    , Compilation_id INTEGER REFERENCES Compilations(id)
-    , CONSTRAINT pk_SongCompilation PRIMARY KEY (
-        Song_id
-        , Compilation_id
-    )
+CREATE TABLE IF NOT EXISTS song_compilation (
+	song_id INTEGER REFERENCES songs(id),
+	compilation_id INTEGER REFERENCES compilations(id),
+	CONSTRAINT pk_song_compilation PRIMARY KEY (
+		song_id,
+		compilation_id
+	)
 );
+
